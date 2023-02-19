ce9ff0ace4f69fa54630c9108ad50d201771f149
Aidin Rasti <aidin.rasti@gmail.com>
2023-02-19 16:34:28 -0500

    add icons

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Readme
 
 A browser extension for Edge and Chrome to search Google Map and Google from context menu.
+
+[Google maps icons created by alfanz - Flaticon](https://www.flaticon.com/free-icons/google-maps)
--- search-google-map/main.js
+++ search-google-map/main.js
@@ -3,23 +3,23 @@
 // found in the LICENSE file.
 
 // When you specify "type": "module" in the manifest background,
-// you can include the service worker as an ES Module,  
+// you can include the service worker as an ES Module,
 
 // Add a listener to create the initial context menu items,
 // context menu items only need to be created at runtime.onInstalled
 chrome.runtime.onInstalled.addListener(async () => {
-    chrome.contextMenus.create({
-      id: 'search-google-map',
-      title: 'Search "%s" on Google Map',
-      contexts: ['selection'],
-      type: 'normal',
-    });
+  chrome.contextMenus.create({
+    id: 'search-google-map',
+    title: 'Search for "%s" on Google Map',
+    contexts: ['selection'],
+    type: 'normal',
+  });
 });
 
 chrome.contextMenus.onClicked.addListener((item, tab) => {
   const id = item.menuItemId;
-if (id === 'search-google-map') {
-    let url = new URL(`https://www.google.com/maps/place/${item.selectionText}`)
+  if (id === 'search-google-map') {
+    let url = new URL(`https://www.google.com/maps/place/${item.selectionText}`);
     chrome.tabs.create({ url: url.href, index: tab.index + 1 });
   }
 });
